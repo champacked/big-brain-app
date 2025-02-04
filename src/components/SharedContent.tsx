6d79a1668f6aa0f2ed69ae4f93c9db8caa3e82ee
Aman Verma <ammy@Amans-MacBook-Air.local>
2025-02-04 20:18:51 +0530

    fixes the shared content error

--- src/components/SharedContent.tsx
+++ src/components/SharedContent.tsx
@@ -21,7 +21,7 @@ function SharedContent() {
   useEffect(() => {
     const fetchSharedContent = async () => {
       try {
-        const response = await axios.get(`${BASE_URL}/shared/${sharelink}`); //here i made the changes
+        const response = await axios.get(`${BASE_URL}/brain/${sharelink}`);
         console.log(response.data);
         setContentStore(response.data.content);
         setDisplayedContent(response.data.content);
@@ -54,14 +54,13 @@ function SharedContent() {
           <ContentBlur sideOpen={sideOpen}>
             <div className="flex flex-col gap-5 px-10 py-10">
               {/* @ts-ignore */}
-              <Heading
-                variant="primary"
-                size={`${isMobile ? "jsm" : "md"}`}
-              >{`${
-                typeof username === "string"
-                  ? username.charAt(0).toUpperCase() + username.slice(1)
-                  : ""
-              }'s Brain`}</Heading>
+              <Heading variant="primary" size={`${isMobile ? "jsm" : "md"}`}>
+                {username
+                  ? `${username.charAt(0).toUpperCase()}${username.slice(
+                      1
+                    )}'s Brain`
+                  : "No username available"}
+              </Heading>
               <SearchBar contentStore={contentStore} />
               <div className="cardsContainer grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-4">
                 {displayedContent.length > 0 ? (
